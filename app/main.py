043915304326afc562a1eb74e4b6a79882f9925c
akrammoni <akrammoni6@gmail.com>
2026-08-02 09:51:54 +0000

    Separate login and dashboard routes

--- app/main.py
+++ app/main.py
@@ -17,11 +17,12 @@ from app.exceptions.validation_error import ValidationError
 
 app = Flask(__name__)
 @app.route("/ui")
+def ui():
+    return render_template("login.html")
+
 @app.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html")
-def ui():
-    return render_template("login.html")
 
 
 # =========================
